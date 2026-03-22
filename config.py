f3efb818548c65c3ab91972a40cdff7a2806e141
Varuni Vangaveti <85639544+Varuni08@users.noreply.github.com>
2026-03-23 01:40:18 +0530

    Add configuration variables for API keys and paths

---
+++ config.py
@@ -0,0 +1,11 @@
+GROQ_API_KEY     = "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"
+PINECONE_API_KEY = "pcsk_7HWBE7_TAZGCnBqboH8YRqBh4FNoS6kpcwnUGiFjSWXRYazmuen44BScTqpfPpWNCa2PSi"
+SUPABASE_URL     = "https://qhdvjompicmxelwrwtzy.supabase.co"
+SUPABASE_KEY     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZHZqb21waWNteGVsd3J3dHp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4ODAxMDMsImV4cCI6MjA4OTQ1NjEwM30.WzVpkUOq7hINtFI3KJw3uhLnfdjojTyfDHsV3zbzVHk"
+
+PINECONE_INDEX   = "reservations"
+EMBED_MODEL      = "all-MiniLM-L6-v2"
+GROQ_MODEL       = "llama-3.3-70b-versatile"
+
+PDF_PATH         = "data/Fake restaurant info.pdf"
+MENU_CSV_PATH    = "data/saffron_table_menu_dataset_ffs_bb.csv"
